9d85073ee5cc0ca5f575b113f380432571c6fbf0
Aditi201022 <79974541+Aditi201022@users.noreply.github.com>
2022-04-28 19:46:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*https://teachablemachine.withgoogle.com/models/1Z4pWKK_G/*/
+/*https://teachablemachine.withgoogle.com/model/1Z4pWKK_G/*/
 
 Webcam.set({
     width:350,
@@ -10,10 +10,33 @@ Webcam.set({
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
-function take_snapshot(){
+function take_snapshot()
+{
     Webcam.snap(function(data_uri){
      document.getElementById("result").innerHTML = '<img id="captured_image" src=" '+data_uri+'"/>'});
 }
 
-console.log('ml5 version:' , ml5.version );
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1Z4pWKK_G/' , modelLoaded);
+console.log('ml5 version' , ml5.version );
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1Z4pWKK_G/model.json' , modelLoaded);
+
+function modelLoaded() 
+{
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.Classify(img, gotResult);
+  }
+
+  function gotResult(error, results) { 
+     if (error) 
+    { console.error(error);
+ } else 
+ {
+     console.log(results);
+     document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+     } 
+    }
